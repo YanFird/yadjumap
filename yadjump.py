7beadc7e4e43e8420a40ec26cd0c45d5bf84d9c1
Yaniv Fridberg <yaniv.fridberg@gmail.com>
2018-01-04 22:18:26 +0200

    added cid scid and oid params which make this tool more dynamic

--- yadjump.py
+++ yadjump.py
@@ -4,7 +4,7 @@
 import requests
 import json
 import argparse
-
+from lxml import html
 
 
 def getcookies(username,password):
@@ -19,34 +19,40 @@ def getcookies(username,password):
     return cookies
 
 
-def getPersonalAreaPage(cookies,url):
+''''
+def getParamsFromPage(cookies):
+
+    url = "https://my.yad2.co.il/newOrder/index.php?action=personalAreaFeed&CatID=3&SubCatID=0"
+
+    page = requests.get(url,cookies=cookies)
+    print  page.content
+    tree = html.fromstring(page.content)
+
+    # This will create a list of buyers:
+
+    cat_id = tree.xpath('//input[@name="CatID"]/@value')
+    print cat_id
+    cat_id = None
+    order_id = None
+    is_trade = None
+    record_id = None
+    state = None
+    pass'''
+
+
+
+''''not in use: only for test
+    def getPersonalAreaPage(cookies,url):
 
     personalArea_url = url
     c = cookies
     r = requests.get(url, cookies=c)
 
-    print r.text
+    print r.text'''
 
-def jumpit(cookies):
+def jumpit(cookies,cid,scid,oid):
 
-
-    url = "https://my.yad2.co.il/newOrder/index.php?action=updateBounceListing&CatID=3&SubCatID=0&OrderID=34616220"
-
-    payload = {
-
-        "action": "updateBounceListing",
-        "CatID": 3,
-        "SubCatID": 0,
-        "OrderID": 34616220,
-        "isTrader": 0,
-        "RecordID": 14227558,
-        "state": 2,
-        "StatusID": 1,
-        "Image1": "o3_0_1_35448_20180103110150.jpg",
-        "Image2": "o3_0_2_35501_20180103110154.jpg",
-    }
-
-
+    url = "https://my.yad2.co.il/newOrder/index.php?action=updateBounceListing&CatID={CID}&SubCatID={SCID}&OrderID={OID}".format(CID=cid,SCID=scid,OID=oid)
     h = {
 
         "Host": "my.yad2.co.il",
@@ -55,34 +61,33 @@ def jumpit(cookies):
         "X-Requested-With": "XMLHttpRequest",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Referer": "https://my.yad2.co.il/newOrder/index.php?action=personalAreaViewDetails&CatID=3&SubCatID=0&OrderID=34616220",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9,he;q=0.8",
         "Pragma": "no - cache"
     }
 
-    #print cookies
-    r = requests.post(url, data=json.dumps(payload),headers=h,cookies=cookies)
-    print r.text
-
+    r = requests.post(url,headers=h,cookies=cookies)
+    if "true" in r.text:
+        print 'Its Works!! Great....'
+    else: print "Oops... something didn't works, Maybe you tried to meany times, please try again later "
+    #print r.text
 
 
 def main():
 
     parser = argparse.ArgumentParser(prog='yadjump',description='Yad2 Autojump tool')
 
-    parser.add_argument('-u', '--username', help='Yad2 username', required=True)
+    parser.add_argument('-u', '--username', help='Yad2 username (your email)', required=True)
     parser.add_argument('-p', '--password',   help='Yad2 password', required=True)
+    parser.add_argument('-cid', '--catid', help='Find CatID in "my ads" page', required=True)
+    parser.add_argument('-scid', '--subcatid', help='Find SubCatID in "my ads" page', required=True)
+    parser.add_argument('-oid', '--orderid', help='Find OrderID in "my ads" page', required=True)
 
     args = parser.parse_args()
 
-    #url = "http://my.yad2.co.il/newOrder/index.php?action=personalAreaIndex"
-    #url = "https://my.yad2.co.il/newOrder/index.php?action=personalAreaFeed&CatID=3&SubCatID=0"
     cookies = getcookies(args.username,args.password)
 
-
-    jumpit(cookies)
-
+    jumpit(cookies,args.catid,args.subcatid,args.orderid)
 
 if __name__ == "__main__":
     main()
